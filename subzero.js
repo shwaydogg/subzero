9dead48b5043bd65811c3c343930afc3cae37437
Gregory Schwedock <schwedock@gmail.com>
2015-11-04 13:32:51 -0500

    Reacting to route change that also uses subZero

--- subzero.js
+++ subzero.js
@@ -1,15 +1,30 @@
 Template.subZero.onCreated(function(){
   var self = this;
-  var subs = FlowRouter.current().route.options.subs;
-  var trackFun = FlowRouter.current().route.options.trackFun;
+  self.theTemplate = new ReactiveVar(self.data.template());
 
-  if(trackFun) self.autorun(function(){
-    trackFun.call(self);
+  self.autorun( () => {
+    var subs = FlowRouter.current().route.options.subs;
+    self.theTemplate.get(); 
+    _.each( subs, sub => self.subscribe.apply(self, sub) );
   });
 
-  _.each( subs, function(sub){
-    self.subscribe.apply(self, sub);
+  self.autorun( () => {
+    var trackFun = FlowRouter.current().route.options.trackFun;
+    self.theTemplate.get(); 
+    if(trackFun) trackFun.call(self);
   });
+
+});
+
+Template.subZero.helpers({
+  theTemplate : function(){
+    //console.log('this:', this, '  instance:',  Template.instance());
+    var instance = Template.instance(), 
+        theTemplate = this.template();
+
+    instance.theTemplate.set(theTemplate);
+    return Template[theTemplate];
+  }
 });
 
 Template.subZero.onRendered(function(){
